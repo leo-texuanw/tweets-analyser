c874956705d3f3f1b0ad568fcdf5281c69635c73
comeonleo <wutexuanleo@outlook.com>
2018-04-29 14:13:16 +0000

    DB: README - add operation commands

--- db/start.sh
+++ db/start.sh
@@ -1,14 +1,5 @@
-chmod +x configure.sh
+sudo docker run -d -p 5984:5984 --env COUCHDB_USER=cluster --env COUCHDB_PASSWORD=cluster12 --volume ~/data:/opt/couchdb/data --name couchdb-cluster couchdb
+#--volume ~/etc/local.d:/opt/couchdb/etc/local.d
 
-docker build -t couchbase-custom .
-
-sleep 5
-
-docker run -d -p 8091-8093:8091-8093 -e COUCHBASE_ADMINISTRATOR_USERNAME=Administrator -e COUCHBASE_ADMINISTRATOR_PASSWORD=password -e COUCHBASE_BUCKET=default -e COUCHBASE_BUCKET_PASSWORD= --network="bridge" --name couchbase1 couchbase-custom
-# docker run -d -p 8094-8096:8091-8093 -e COUCHBASE_ADMINISTRATOR_USERNAME=Administrator -e COUCHBASE_ADMINISTRATOR_PASSWORD=password -e COUCHBASE_BUCKET=default -e COUCHBASE_BUCKET_PASSWORD= --network="bridge" --name couchbase2 couchbase-custom
-# docker run -d -p 8097-8099:8091-8093 -e COUCHBASE_ADMINISTRATOR_USERNAME=Administrator -e COUCHBASE_ADMINISTRATOR_PASSWORD=password -e COUCHBASE_BUCKET=default -e COUCHBASE_BUCKET_PASSWORD= --network="bridge" --name couchbase3 couchbase-custom
-
-# Or add in cli:
-#     exec `docker exec -it couchbase1 bash` to enter
-# input:
-#     /opt/couchbase/bin/couchbase-cli server-add --cluster=localhost:8091 --user Administrator --password password --server-add 172.17.0.3 --server-add-username Administrator --server-add-password password
+# add node in cli:
+#     exec `docker exec -it couchdb bash` to enter
