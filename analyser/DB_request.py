4e7c11d30fd333885f5d55df20ed3bc0a774b193
Yujing Mark Jiang <yujingj@student.unimelb.edu.au>
2018-05-07 11:08:01 +1000

    DB sentiment update

--- analyser/DB_request.py
+++ analyser/DB_request.py
@@ -7,7 +7,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pickle
 
 # Parameter(s) for the sentiment classifier
-NEG_THRESHOLD = 0.1
+NEG_THRESHOLD = 0.05
 
 # Parameter(s) for DB request
 DB_USERNAME = 'cluster'
@@ -33,7 +33,9 @@ for db_name in all_dbs:
     # Detect if the DB is the (researved) info DB
     if db_name == INFO_DB_NAME:
         continue
+
     
+    print('>'*20 + db_name + '<'*20 )
     lang_dict[db_name] = {'pos': 0, 'neg': 0, 'total': 0}
     
     r = requests.get(DB_URL + '%s/_all_docs' % \
@@ -48,13 +50,17 @@ for db_name in all_dbs:
     
     L = []
 
-    for uuid in UID[:100]:
+    # Get all items in the sub_db by uid
+    for uuid in UID:
         url = DB_URL + db_name + '/'+str(uuid)
-        print(url)
+        #print(url)
         r = requests.get(url)
         L.append(r.json())
-        if 1:
-            raw_str = L[-1]['info'].encode('ascii', 'ignore')
+
+        json_dict = L[-1]
+        
+        if json_dict:
+            raw_str = json_dict['info'].encode('ascii', 'ignore')
             msg = re.findall('''\"msg\"\:\"([^\"]*)\"''', str(raw_str))[0]
             #msg = json.loads(L[-1]['info'].encode('ascii', 'ignore'))['msg']
             
@@ -62,8 +68,16 @@ for db_name in all_dbs:
             lang_dict[db_name]['total'] += 1
             if ss['neg'] > ss['pos'] and ss['neg'] > NEG_THRESHOLD:
                 lang_dict[db_name]['neg'] += 1
-                print(msg)
-                print(ss, end = '\n\n')
+                json_dict['sentiment'] = 'neg'
+                print('Sentiment: neg')
             else:
                 lang_dict[db_name]['pos'] += 1
+                json_dict['sentiment'] = 'pos'
+                print('Sentiment: pos')
+
+            # Update DB
+            r = requests.put(url, json.dumps(json_dict))
+            if not r.json()['ok']:
+                print('#'*20 + ' ERROR ' + '#'*20)
+                print(r.json())
                 
